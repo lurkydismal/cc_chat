2b479198f57937cf0edffdf965a34f4503dcb0d9
lurkydismal <edruska@mail.ru>
2022-04-25 06:27:05 -0700

    refactoring

--- tools/tool_useradd.cpp
+++ tools/tool_useradd.cpp
@@ -4,46 +4,79 @@
 #include <soci/soci.h>
 #include <soci/mysql/soci-mysql.h>
 #include <stdexcept>
+#include <vector>
 
-void usage()
-{
-    std::cout   << "tool_useradd usage:" << std::endl
-                << "./tool_useradd <login> <password>" << std::endl << std::endl;
+void printUsage( void ) {
+    std::cout
+        << "tool_useradd usage:"               << std::endl
+        << "./tool_useradd <login> <password>" << std::endl
+    << std::endl;
 }
 
-int main(int argc, char** argv)
-{
-    if (argc < 3)
-    {
-        usage();
-        return 1;
-    }
-    std::string login = argv[1];
-    std::string passwd = argv[2];
-    std::string encoded_passwd;
+int main( const int argc, const char* argv[] ) {
+    if ( argc < 3 ) {
+        printUsage();
 
-    CryptoPP::SHA512 encoder;
-    uint8_t encoded_buff[CryptoPP::SHA512::DIGESTSIZE];
-    encoder.CalculateDigest(encoded_buff, reinterpret_cast<const uint8_t*>(passwd.c_str()), passwd.size());
-    CryptoPP::HexEncoder hex_to_str(new CryptoPP::StringSink(encoded_passwd));
-    hex_to_str.Put(encoded_buff, CryptoPP::SHA512::DIGESTSIZE);
-    hex_to_str.MessageEnd();
+        return ( EXIT_FAILURE );
+    }
 
-    try
-    {
-        soci::session sql(soci::mysql, std::string("db=") + MYSQL_DB + " user=" + MYSQL_USER + " password='" + MYSQL_PASSWD + "' host=" + MYSQL_HOST + " port=" + std::to_string(MYSQL_PORT));
-        if (!sql.is_connected())
-            throw std::runtime_error("Unable to connect to mysql server");
-        std::string temp;
-        sql.once << "select user from users where user=:login", soci::use(login), soci::into(temp);
-        if (!temp.empty())
-            throw std::runtime_error("User exists");
-        sql.once << "insert into users(user, passwd) values(:login, :passwd)", soci::use(login), soci::use(encoded_passwd);
+    std::string encodedPassword;
+    uint8_t     encodedBuffer[ CryptoPP::SHA512::DIGESTSIZE ];
+    const std::string            dbLogin    = argv[ 1 ];
+    const std::vector< uint8_t > dbPassword = argv[ 2 ];
+
+    ( new CryptoPP::SHA512 ).CalculateDigest(
+        encodedBuffer,
+        dbPassword.data(),
+        dbPassword.size(),
+    );
+    CryptoPP::HexEncoder hexEncoder(
+        new CryptoPP::StringSink( encodedPassword )
+    );
+    hexEncoder.Put(
+        encodedBuffer,
+        CryptoPP::SHA512::DIGESTSIZE
+    );
+    hexEncoder.MessageEnd();
+    soci::session SQLConnection(
+        soci::mysql,
+        (
+            "db=" +
+            MYSQL_DB +
+            " user=" +
+            MYSQL_USER +
+            " password=\'" +
+            MYSQL_PASSWD +
+            "\' host=" +
+            MYSQL_HOST +
+            " port=" +
+            std::to_string( MYSQL_PORT )
+        )
+    );
+
+    if ( !SQLConnection.is_connected() ) {
+        throw (
+            std::runtime_error( "Unable to connect to mysql server" )
+        );
     }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-        return 1;
+
+    std::string tableContent;
+
+    SQLConnection.once
+        << "select user from users where user=:login",
+        soci::use( dbLogin ),
+        soci::into( tableContent );
+
+    if ( !tableContent.empty() ) {
+        throw (
+            std::runtime_error( "User exists" )
+        );
     }
-    return 0;
+
+    SQLConnection.once
+        << "insert into users( user, passwd ) values( :login, :passwd )",
+        soci::use( dbLogin ),
+        soci::use( encodedPassword );
+
+    return ( EXIT_SUCCESS );
 }
