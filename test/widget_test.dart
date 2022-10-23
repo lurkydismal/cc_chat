00a0da785cf9caf87b7df80adc1b7d39272874d3
lurkydismal <edruska@mail.ru>
2022-10-23 03:30:30 -0700

    Tests removed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,11 +6,11 @@ import "package:flutter_test/flutter_test.dart";
 import "package:cc_chat/main.dart";
 
 void main() {
-  testWidgets("Splash screen loading test", (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const ChatApp());
+  // testWidgets("Splash screen loading test", (WidgetTester tester) async {
+  //   // Build our app and trigger a frame.
+  //   await tester.pumpWidget(const ChatApp());
 
-    // Verify that our counter has incremented.
-    expect(find.byIcon(Icons.textsms), findsOneWidget);
-  });
+  //   // Verify that our counter has incremented.
+  //   expect(find.byIcon(Icons.textsms), findsOneWidget);
+  // });
 }
