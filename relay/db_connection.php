60307bb26ceb3a81d57b2d0dc0d662e202fafdc0
lurkydismal <lurkydismal@duck.com>
2023-08-07 00:18:03 -0700

    Back to normal

--- relay/db_connection.php
+++ relay/db_connection.php
@@ -13,6 +13,8 @@ $db_connection = new mysqli(
 );
 
 if ( $db_connection->connect_errno !== 0 ) {
+    echo "Connection failed: " . $db_connection->connect_error;
+
     die( "Connection failed: " . $db_connection->connect_error );
 }
 
