c420e602d98e6d00cdf5024c838313aef9c9be28
lurkydismal <lurkydismal@duck.com>
2023-08-07 00:15:11 -0700

    Remove echo

--- relay/db_connection.php
+++ relay/db_connection.php
@@ -13,8 +13,6 @@ $db_connection = new mysqli(
 );
 
 if ( $db_connection->connect_errno !== 0 ) {
-    echo "Connection failed: " . $db_connection->connect_error;
-
     die( "Connection failed: " . $db_connection->connect_error );
 }
 
